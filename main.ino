a50a52a6dc37bc065f63c8fa50208bc570467bd5
2018macbookair <62246598+Mateo-Johnson@users.noreply.github.com>
2024-10-30 21:31:16 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,16 +2,13 @@
 
 const int MPU_ADDR = 0x68;
 
-int16_t accelerometer_x, accelerometer_y, accelerometer_z;
-int16_t gyro_x, gyro_y, gyro_z;
-int16_t temperature;
+int16_t acc_x, acc_y, acc_z;
+int16_t rot_x, rot_y, rot_z;
+int16_t temp;
 
-float kalman_angle_x, kalman_angle_y;
-float kalman_bias_x, kalman_bias_y;
-float P[2][2] = {{1, 0}, {0, 1}};
-float Q_angle = 0.001, Q_bias = 0.003, R_measure = 0.03;
+float kAngle_x, kAngle_y;
 
-float accel_offset_x = 0, accel_offset_y = 0, accel_offset_z = 0;
+float accoff_x = 0, accoff_y = 0, accoff_z = 0;
 float gyro_offset_x = 0, gyro_offset_y = 0, gyro_offset_z = 0;
 
 char tmp_str[7];
@@ -35,17 +32,17 @@ void calibrate() {
     ax_sum += (Wire.read() << 8 | Wire.read());
     ay_sum += (Wire.read() << 8 | Wire.read());
     az_sum += (Wire.read() << 8 | Wire.read());
-    Wire.read(); Wire.read(); // Skip temperature
+    Wire.read(); Wire.read();
     gx_sum += (Wire.read() << 8 | Wire.read());
     gy_sum += (Wire.read() << 8 | Wire.read());
     gz_sum += (Wire.read() << 8 | Wire.read());
 
-    delay(3); // Small delay for stability
+    delay(3);
   }
 
-  accel_offset_x = ax_sum / samples;
-  accel_offset_y = ay_sum / samples;
-  accel_offset_z = az_sum / samples;
+  accoff_x = ax_sum / samples;
+  accoff_y = ay_sum / samples;
+  accoff_z = az_sum / samples;
   gyro_offset_x = gx_sum / samples;
   gyro_offset_y = gy_sum / samples;
   gyro_offset_z = gz_sum / samples;
@@ -66,7 +63,7 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission(true);
 
-  calibrate(); // Call the calibration routine
+  calibrate(); 
 }
 
 void loop() {
@@ -75,57 +72,61 @@ void loop() {
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_ADDR, 14, true);
 
-  accelerometer_x = (Wire.read() << 8 | Wire.read()) - accel_offset_x;
-  accelerometer_y = (Wire.read() << 8 | Wire.read()) - accel_offset_y;
-  accelerometer_z = (Wire.read() << 8 | Wire.read()) - accel_offset_z;
-  temperature = Wire.read() << 8 | Wire.read();
-  gyro_x = (Wire.read() << 8 | Wire.read()) - gyro_offset_x;
-  gyro_y = (Wire.read() << 8 | Wire.read()) - gyro_offset_y;
-  gyro_z = (Wire.read() << 8 | Wire.read()) - gyro_offset_z;
+  acc_x = (Wire.read() << 8 | Wire.read()) - accoff_x;
+  acc_y = (Wire.read() << 8 | Wire.read()) - accoff_y;
+  acc_z = (Wire.read() << 8 | Wire.read()) - accoff_z;
+  temp = Wire.read() << 8 | Wire.read();
+  rot_x = (Wire.read() << 8 | Wire.read()) - gyro_offset_x;
+  rot_y = (Wire.read() << 8 | Wire.read()) - gyro_offset_y;
+  rot_z = (Wire.read() << 8 | Wire.read()) - gyro_offset_z;
 
   float dt = 0.01;
-  float accel_angle_x = atan2(accelerometer_y, accelerometer_z) * 180 / 3.14159265359;
-  float accel_angle_y = atan2(accelerometer_x, accelerometer_z) * 180 / 3.14159265359;
+  float accel_angle_x = atan2(acc_y, acc_z) * 180 / 3.14159265359;
+  float accel_angle_y = atan2(acc_x, acc_z) * 180 / 3.14159265359;
 
-  float gyro_rate_x = gyro_x / 131.0;
-  float gyro_rate_y = gyro_y / 131.0;
+  float gyro_rate_x = rot_x / 131.0;
+  float gyro_rate_y = rot_y / 131.0;
 
-  kalman_angle_x = complementary_filter(accel_angle_x, gyro_rate_x, dt, &kalman_angle_x);
-  kalman_angle_y = complementary_filter(accel_angle_y, gyro_rate_y, dt, &kalman_angle_y);
+  kAngle_x = complementary_filter(accel_angle_x, gyro_rate_x, dt, &kAngle_x);
+  kAngle_y = complementary_filter(accel_angle_y, gyro_rate_y, dt, &kAngle_y);
 
-  Serial.print("Tilt direction: ");
+  Serial.print("Direction: ");
   
-  if (kalman_angle_x > 10) {
-    Serial.print("Tilted Forward ");
-  } else if (kalman_angle_x < -10) {
-    Serial.print("Tilted Backward ");
+  if (kAngle_x > 10) {
+    Serial.print("Tilted Forward (Angle: ");
+    Serial.print(kAngle_x);
+    Serial.print("°) ");
+  } else if (kAngle_x < -10) {
+    Serial.print("Tilted Backward (Angle: ");
+    Serial.print(kAngle_x);
+    Serial.print("°) ");
   }
 
-  if (kalman_angle_y > 10) {
-    Serial.print("Tilted Right ");
-  } else if (kalman_angle_y < -10) {
-    Serial.print("Tilted Left ");
-  }
-
-  if (abs(kalman_angle_x) <= 10 && abs(kalman_angle_y) <= 10) {
-    Serial.print("Stable ");
+  if (kAngle_y > 10) {
+    Serial.print("Tilted Right (Angle: ");
+    Serial.print(kAngle_y);
+    Serial.print("°) ");
+  } else if (kAngle_y < -10) {
+    Serial.print("Tilted Left (Angle: ");
+    Serial.print(kAngle_y);
+    Serial.print("°) ");
   }
 
   Serial.print(" | Acceleration: ");
   
-  if (accelerometer_x > 3000) {
+  if (acc_x > 3000) {
     Serial.print("Accelerating Right ");
-  } else if (accelerometer_x < -3000) {
+  } else if (acc_x < -3000) {
     Serial.print("Accelerating Left ");
   }
 
-  if (accelerometer_y > 3000) {
+  if (acc_y > 3000) {
     Serial.print("Accelerating Forward ");
-  } else if (accelerometer_y < -3000) {
+  } else if (acc_y < -3000) {
     Serial.print("Accelerating Backward ");
   }
 
-  if (abs(accelerometer_x) <= 3000 && abs(accelerometer_y) <= 3000) {
+  if ((abs(kAngle_x) <= 10 && abs(kAngle_y) <= 10) && (abs(acc_x) <= 3000 && abs(acc_y) <= 3000)) {
     Serial.print("Stable ");
   }
 
